b2c952eea74d1e53c76008d467987b280a7e8080
Anton Iun <iunanton@naver.com>
2017-09-22 22:37:46 +0800

    Use bcrypt for users' password hashing

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
 const app = express();
+const bcrypt = require('bcrypt');
 var mongo = require('mongodb').MongoClient;
 var assert = require('assert');
 var url = 'mongodb://mongo:27017/chat';
@@ -59,10 +60,10 @@ wss.on('connection', function connection(ws, req) {
 		type = JSON.parse(event).type;
 		switch(type) {
 			case "ping":
-				console.log("%s Received PING", Date.now());
+				// console.log("%s Received PING", Date.now());
 				var message = JSON.stringify({ "type": "pong", "data": {} });
 				ws.send(message);
-				console.log("%s Transmitted PONG", Date.now());
+				// console.log("%s Transmitted PONG", Date.now());
 				break;
 			case "guest":
 				var data = JSON.parse(event).data;
@@ -173,41 +174,22 @@ wss.on('connection', function connection(ws, req) {
 						db.collection("users").findOne({ "isDeleted": false, "username": data.username }, { "password": 1, "isOnline": 1 }, function (err, r) {
 							if (!err) {
 								// console.log("%s Check password...", Date.now());
-								if (r && data.password === r.password) {
-									// console.log("%s isOnline?...", Date.now());
-									if (r.isOnline) {
-										// console.log("%s isOnline: true, close last connection...", Date.now());
-										wss.clients.forEach(function each(client) { 
-											if(r._id.equals(client.uuid)) {
-												client.authenticated = false;
-												client.close();
-											}
-										});
-										ws.authenticated = true;
-										ws.uuid = r._id;
-										console.log("%s Connection authenticated: uuid: %s", Date.now(), ws.uuid);
-										db.collection("users").find({ "isOnline": true }, { "isGuest": 1, "isDelete": 1, "isOnline": 1, "username": 1 }).toArray(function(err, users) {
-											if (!err) {
-												db.collection("messages").find().toArray(function(err, messages) {
-													if (!err) {
-														var message = JSON.stringify({ "type": "context", "data": { "users": users, "messages": messages } } );
-														ws.send(message);
-													} else {
-														var message = JSON.stringify({ "type": "error", "data": { "reason": err.name } });
-														ws.send(message);
+								if (r) {
+									// console.log("%s user was found...", Date.now());
+									// console.log("%s receive: %s, in DB: %s", Date.now(), data.password, r.password);
+									bcrypt.compare(data.password, r.password, function(err, res) {
+										if(res) {
+											// console.log("%s isOnline?...", Date.now());
+											if (r.isOnline) {
+												// console.log("%s isOnline: true, close last connection...", Date.now());
+												wss.clients.forEach(function each(client) {
+													if(r._id.equals(client.uuid)) {
+														client.authenticated = false;
+														client.close();
 													}
 												});
-											} else {
-												var message = JSON.stringify({ "type": "error", "data": { "reason": err.name } });
-												ws.send(message);
-											}
-										});
-									} else {
-										// console.log("%s isOnline: false, set isOnline to true...", Date.now());
-										ws.authenticated = true;
-										ws.uuid = r._id;
-										db.collection("users").updateOne({ "_id": ws.uuid }, { $set: { "isOnline": true } }, function (err, r) {
-											if (!err) {
+												ws.authenticated = true;
+												ws.uuid = r._id;
 												console.log("%s Connection authenticated: uuid: %s", Date.now(), ws.uuid);
 												db.collection("users").find({ "isOnline": true }, { "isGuest": 1, "isDelete": 1, "isOnline": 1, "username": 1 }).toArray(function(err, users) {
 													if (!err) {
@@ -225,19 +207,48 @@ wss.on('connection', function connection(ws, req) {
 														ws.send(message);
 													}
 												});
-												// console.log("%s Broadcasting userJoined...", Date.now());
-												var user = { "userUuid": ws.uuid, "isGuest": false, "isDeleted": false, "isOnline": true, "username": data.username };
-												var broadcast = JSON.stringify({ "type": "userJoined", "data": user });
-												wss.clients.forEach(function each(client) {
-													if (client.readyState === WebSocket.OPEN && client.authenticated && client !== ws)
-														client.send(broadcast);
-												});
 											} else {
-												var message = JSON.stringify({ "type": "error", "data": { "reason": err.name } });
-												ws.send(message);
+												// console.log("%s isOnline: false, set isOnline to true...", Date.now());
+												ws.authenticated = true;
+												ws.uuid = r._id;
+												db.collection("users").updateOne({ "_id": ws.uuid }, { $set: { "isOnline": true } }, function (err, r) {
+													if (!err) {
+														console.log("%s Connection authenticated: uuid: %s", Date.now(), ws.uuid);
+														db.collection("users").find({ "isOnline": true }, { "isGuest": 1, "isDelete": 1, "isOnline": 1, "username": 1 }).toArray(function(err, users) {
+															if (!err) {
+																db.collection("messages").find().toArray(function(err, messages) {
+																	if (!err) {
+																		var message = JSON.stringify({ "type": "context", "data": { "users": users, "messages": messages } } );
+																		ws.send(message);
+																	} else {
+																		var message = JSON.stringify({ "type": "error", "data": { "reason": err.name } });
+																		ws.send(message);
+																	}
+																});
+															} else {
+																var message = JSON.stringify({ "type": "error", "data": { "reason": err.name } });
+																ws.send(message);
+															}
+														});
+														// console.log("%s Broadcasting userJoined...", Date.now());
+														var user = { "userUuid": ws.uuid, "isGuest": false, "isDeleted": false, "isOnline": true, "username": data.username };
+														var broadcast = JSON.stringify({ "type": "userJoined", "data": user });
+														wss.clients.forEach(function each(client) {
+															if (client.readyState === WebSocket.OPEN && client.authenticated && client !== ws)
+																client.send(broadcast);
+														});
+													} else {
+														var message = JSON.stringify({ "type": "error", "data": { "reason": err.name } });
+														ws.send(message);
+													}
+												});
 											}
-										});
-									}
+										} else {
+											// console.log("%s Not match, access denied...", Date.now());
+											var message = JSON.stringify({ "type": "error", "data": { "reason": "Username or password incorrect." } });
+											ws.send(message);
+										}
+									});
 								} else {
 									var message = JSON.stringify({ "type": "error", "data": { "reason": "Username or password incorrect." } });
 									ws.send(message);
@@ -261,37 +272,41 @@ wss.on('connection', function connection(ws, req) {
 							db.collection("users").findOne({ "username": data.username }, { "isGuest": 1, "isOnline": 1 }, function (err, r) {
 								if (!err) {
 									if (!r) {
-										var user = { "userUuid": ws.uuid, "isGuest": false, "isDeleted": false, "isOnline": true, "username": data.username, "password": data.password };
-										db.collection("users").insertOne( user, function(err, r) {
-											if (!err) {
-												ws.authenticated = true;
-												ws.uuid = user._id;
-												console.log("%s Connection authenticated: uuid: %s", Date.now(), ws.uuid);
-												db.collection("users").find({ "isOnline": true }, { "isGuest": 1, "isDelete": 1, "isOnline": 1, "username": 1 }).toArray(function(err, users) {
-													if (!err) {
-														db.collection("messages").find().toArray(function(err, messages) {
-															if (!err) {
-																var message = JSON.stringify({ "type": "context", "data": { "users": users, "messages": messages } } );
-																ws.send(message);
-															} else {
-																var message = JSON.stringify({ "type": "error", "data": { "reason": err.name } });
-																ws.send(message);
-															}
-														});
-													} else {
-														var message = JSON.stringify({ "type": "error", "data": { "reason": err.name } });
-														ws.send(message);
-													}
-												});
-												var broadcast = JSON.stringify({ "type": "userJoined", "data": user });
-												wss.clients.forEach(function each(client) {
-													if (client.readyState === WebSocket.OPEN && client.authenticated && client !== ws)
-														client.send(broadcast);
-												});
-											} else {
-												var message = JSON.stringify({ "type": "error", "data": { "reason": err.name } });
-												ws.send(message);
-											}
+										// console.log("%s Try to compute hash...", Date.now());
+										bcrypt.hash(data.password, 10, function(err, hash) {
+											// console.log("%s hash: %s", Date.now(), hash);
+											var user = { "userUuid": ws.uuid, "isGuest": false, "isDeleted": false, "isOnline": true, "username": data.username, "password": hash };
+											db.collection("users").insertOne( user, function(err, r) {
+												if (!err) {
+													ws.authenticated = true;
+													ws.uuid = user._id;
+													console.log("%s Connection authenticated: uuid: %s", Date.now(), ws.uuid);
+													db.collection("users").find({ "isOnline": true }, { "isGuest": 1, "isDelete": 1, "isOnline": 1, "username": 1 }).toArray(function(err, users) {
+														if (!err) {
+															db.collection("messages").find().toArray(function(err, messages) {
+																if (!err) {
+																	var message = JSON.stringify({ "type": "context", "data": { "users": users, "messages": messages } } );
+																	ws.send(message);
+																} else {
+																	var message = JSON.stringify({ "type": "error", "data": { "reason": err.name } });
+																	ws.send(message);
+																}
+															});
+														} else {
+															var message = JSON.stringify({ "type": "error", "data": { "reason": err.name } });
+															ws.send(message);
+														}
+													});
+													var broadcast = JSON.stringify({ "type": "userJoined", "data": user });
+													wss.clients.forEach(function each(client) {
+														if (client.readyState === WebSocket.OPEN && client.authenticated && client !== ws)
+															client.send(broadcast);
+													});
+												} else {
+													var message = JSON.stringify({ "type": "error", "data": { "reason": err.name } });
+													ws.send(message);
+												}
+											});
 										});
 									} else {
 										var message = JSON.stringify({ "type": "error", "data": { "reason": "This username is already in use." } });
