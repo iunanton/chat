bc08981d36874349ba8bfd9496d3e7531abecc08
Anton Iun <iunanton@naver.com>
2017-10-04 02:36:38 +0800

    Update scripts

--- app.js
+++ app.js
@@ -44,22 +44,22 @@ var webSockOpts = {
 const wss = new WebSocket.Server(webSockOpts);
 
 wss.on('connection', function connection(ws, req) {
-	
-	ws.token = URL.parse(req.url, true).query.access_token;
-	mongo.connect(url, function(err, db) {	
+	var token = URL.parse(req.url, true).query.access_token;
+	mongo.connect(url, function(err, db) {
 		console.log("%s Connection established: total clients: %d", Date.now(), wss.clients.size);
-		db.collection("users").find({ "isOnline": true }, { "username": 1 }).toArray(function (err, users) {
-			db.collection("messages").find().toArray(function(err, messages) {
-				var message = JSON.stringify({ "type": "context", "data": { "users": users, "messages": messages } } );
-				ws.send(message);
-				db.collection("users").updateOne({ "tokens": { $elemMatch: { $eq: ws.token } } }, { $set: { "isOnline": true } }, function (err, r) {
-					db.collection("users").findOne({ "tokens": { $elemMatch: { $eq: ws.token } } }, { "isGuest": 1, "isDeleted": 1, "isOnline": 1, "username": 1 }, function (err, r) {
-						var user = { "userUuid": ws.token, "isGuest": r.isGuest, "isDeleted": r.isDelete, "isOnline": r.isOnline, "username": r.username };
-						var broadcast = JSON.stringify({ "type": "userJoined", "data": user });
-						wss.clients.forEach(function each(client) {
-							if (client.readyState === WebSocket.OPEN)
-								client.send(broadcast);
-						});
+		db.collection("users").updateOne({ "tokens": { $elemMatch: { $eq: token } } }, { $set: { "isOnline": true } }, function (err, r) {
+			db.collection("users").findOne({ "tokens": { $elemMatch: { $eq: token } } }, { "isGuest": 1, "isDeleted": 1, "isOnline": 1, "username": 1 }, function (err, user) {
+				ws.uuid = user._id;
+				var broadcast = JSON.stringify({ "type": "userJoined", "data": user });
+				wss.clients.forEach(function each(client) {
+					if (client.readyState === WebSocket.OPEN && client !== ws) {
+						client.send(broadcast);
+					}
+				});
+				db.collection("users").find({ "isOnline": true }, { "isGuest": 1, "isDeleted": 1, "isOnline": 1, "username": 1 }).toArray(function (err, users) {
+					db.collection("messages").find().toArray(function(err, messages) {
+						var message = JSON.stringify({ "type": "context", "data": { "users": users, "messages": messages } } );
+						ws.send(message);
 					});
 				});
 			});
@@ -69,10 +69,10 @@ wss.on('connection', function connection(ws, req) {
 	ws.on('close', function(event) {
 		console.log("%s Connection closed: uuid: %s, total clients: %d", Date.now(), ws.uuid, wss.clients.size);
 		mongo.connect(url, function(err, db) {
-			db.collection("users").updateOne({ "tokens": { $elemMatch: { $eq: ws.token } } }, { $set: { "isOnline": false } }, function (err, r) {
-				var broadcast = JSON.stringify({ "type": "userLeft", "data": { "token": ws.token } });
+			db.collection("users").updateOne({ "_id": ws.uuid }, { $set: { "isOnline": false } }, function (err, r) {
+				var broadcast = JSON.stringify({ "type": "userLeft", "data": { "_id": ws.uuid } });
 				wss.clients.forEach(function each(client) {
-					if (client.readyState === WebSocket.OPEN && client.authenticated && client !== ws)
+					if (client.readyState === WebSocket.OPEN && client !== ws)
 						client.send(broadcast);
 				});
 			});
@@ -85,8 +85,8 @@ wss.on('connection', function connection(ws, req) {
 			case "message":	
 				var data = JSON.parse(event).data;
 				mongo.connect(url, function(err, db) {
-					db.collection("users").findOne({ "tokens": { $elemMatch: { $eq: ws.token } } }, { "username": 1 }, function (err, r) {
-						var message = { "userUuid": ws.token, "messageBody": data.messageBody, "timestamp": Date.now(), "username": r.username };
+					db.collection("users").findOne({ "_id": ws.uuid }, { "username": 1 }, function (err, r) {
+						var message = { "_id": ws.uuid, "messageBody": data.messageBody, "timestamp": Date.now(), "username": r.username };
 						var broadcast = JSON.stringify({ "type": "messageAdd", "data": message });
 						db.collection("messages").insertOne( message, function(err, r) {	
 							wss.clients.forEach(function each(client) {
